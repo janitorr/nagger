79a387a53577ba40057e23d804f3981fcf07c7bd
Jani Similä <15038593+janitorr@users.noreply.github.com>
2026-08-03 13:56:56 +0300

    Separate reminder policy model

--- src/Nagger.Core/Tasks/TaskItem.cs
+++ src/Nagger.Core/Tasks/TaskItem.cs
@@ -63,38 +63,6 @@ public enum OneShotTaskStatus
     Cancelled
 }
 
-public enum ReminderPolicy
-{
-    None,
-    Once,
-    WeeklyUntilDone
-}
-
-public static class ReminderPolicies
-{
-    public static bool TryParse(string? value, out ReminderPolicy policy) => value switch
-    {
-        "none" => Set(ReminderPolicy.None, out policy),
-        "once" => Set(ReminderPolicy.Once, out policy),
-        "weekly-until-done" => Set(ReminderPolicy.WeeklyUntilDone, out policy),
-        _ => Set(default, out policy, false)
-    };
-
-    public static string ToContractValue(this ReminderPolicy policy) => policy switch
-    {
-        ReminderPolicy.None => "none",
-        ReminderPolicy.Once => "once",
-        ReminderPolicy.WeeklyUntilDone => "weekly-until-done",
-        _ => throw new ArgumentOutOfRangeException(nameof(policy))
-    };
-
-    private static bool Set(ReminderPolicy value, out ReminderPolicy policy, bool success = true)
-    {
-        policy = value;
-        return success;
-    }
-}
-
 public static class OneShotTaskStatuses
 {
     public static string ToContractValue(this OneShotTaskStatus status) => status switch
